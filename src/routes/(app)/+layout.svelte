25be7617e0d3174927143174769512d2e840e9ea
Daniel Pikovsky <dan13161316@gmail.com>
2025-09-30 11:03:25 +0300

    fix (layout): temporary removing changelog modal

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -271,7 +271,7 @@
 </script>
 
 <SettingsModal bind:show={$showSettings} />
-<ChangelogModal bind:show={$showChangelog} />
+<!-- <ChangelogModal bind:show={$showChangelog} /> -->
 
 {#if version && compareVersion(version.latest, version.current) && ($settings?.showUpdateToast ?? true)}
 	<div class=" absolute bottom-8 right-8 z-50" in:fade={{ duration: 100 }}>
